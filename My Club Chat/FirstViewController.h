a73c26417cea87b58aaf6af54ba9545cea4165e7
Sukhpal <sukhpal@Sukhpals-Mac-mini.local>
2013-04-11 21:18:39 +0530

    Initial Commit

---
+++ My Club Chat/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  My Club Chat
+//
+//  Created by Sukhpal on 4/11/13.
+//  Copyright (c) 2013 My Club Chat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ My Club Chat/FirstViewController.m
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.m
+//  My Club Chat
+//
+//  Created by Sukhpal on 4/11/13.
+//  Copyright (c) 2013 My Club Chat. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
